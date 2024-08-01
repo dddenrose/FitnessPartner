b2c71d29ee6dfd26187ac5069f48544918d52fe5
Chi Yang <dddenrose@gmail.com>
2024-08-01 23:35:58 +0800

    set timer logic

--- app/workout/_components/WorkoutProcess/page.tsx
+++ app/workout/_components/WorkoutProcess/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import LinearProgress, {
   LinearProgressProps,
 } from "@mui/material/LinearProgress";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
 function LinearProgressWithLabel(
   props: LinearProgressProps & { value: number }
@@ -12,12 +12,11 @@ function LinearProgressWithLabel(
       <Box sx={{ width: "100%", mr: 1 }}>
         <LinearProgress variant="determinate" {...props} />
       </Box>
-      <Box sx={{ minWidth: 35 }}>
-        <Typography variant="body2" color="text.secondary">{`${Math.round(
-          props.value
-        )}
-        seconds`}</Typography>
-      </Box>
+      <div className="w-20">
+        <Typography variant="body2" color="text.secondary">
+          {`${Math.round(props.value)} / 100s`}
+        </Typography>
+      </div>
     </Box>
   );
 }
@@ -25,81 +24,141 @@ function LinearProgressWithLabel(
 const aerobicsList = [
   {
     title: "Walking Lunges",
-    execriseTimes: 60,
-    times: 10,
-    restTimes: 30,
+    execriseTimes: 6,
+    times: 1,
+    restTimes: 4,
   },
   {
     title: "Burpees",
-    execriseTimes: 60,
-    times: 10,
-    restTimes: 30,
+    execriseTimes: 6,
+    times: 1,
+    restTimes: 4,
   },
   {
     title: "Jumping Jacks",
-    execriseTimes: 60,
-    times: 10,
-    restTimes: 30,
+    execriseTimes: 6,
+    times: 1,
+    restTimes: 4,
   },
   {
     title: "Mountain Climbers",
-    execriseTimes: 60,
-    times: 5,
-    restTimes: 30,
+    execriseTimes: 6,
+    times: 1,
+    restTimes: 4,
   },
   {
     title: "High Knees",
-    execriseTimes: 60,
-    times: 5,
-    restTimes: 30,
+    execriseTimes: 6,
+    times: 1,
+    restTimes: 4,
   },
   {
     title: "Butt Kicks",
-    execriseTimes: 60,
-    times: 5,
-    restTimes: 30,
+    execriseTimes: 6,
+    times: 1,
+    restTimes: 4,
   },
   {
     title: "Skaters",
-    execriseTimes: 60,
-    times: 5,
-    restTimes: 30,
+    execriseTimes: 6,
+    times: 1,
+    restTimes: 4,
   },
   {
     title: "Plank Jacks",
-    execriseTimes: 60,
-    times: 5,
-    restTimes: 30,
+    execriseTimes: 6,
+    times: 1,
+    restTimes: 4,
   },
 ];
 
 const WorkoutProcess: React.FC = () => {
-  const [execriseList, setExecriseList] = React.useState(aerobicsList);
+  const [execriseList, setExecriseList] = React.useState<any[]>(aerobicsList);
+  const [isPause, setIsPause] = React.useState(false);
   const [progress, setProgress] = React.useState(0);
 
   React.useEffect(() => {
     if (!execriseList?.length) return;
 
     const timer = setInterval(() => {
-      const slot = 100 / aerobicsList[0].execriseTimes;
+      if (isPause) return;
 
-      setProgress((prevProgress) => (prevProgress += slot));
+      const result = execriseList
+        ?.map((item, index) => {
+          if (index !== 0) return item;
+
+          if (item?.execriseTimes) {
+            return {
+              ...item,
+              execriseTimes: item.execriseTimes - 1,
+            };
+          } else if (item?.restTimes) {
+            return {
+              ...item,
+              restTimes: item.restTimes - 1,
+            };
+          } else if (item?.times) {
+            return {
+              ...item,
+              times: item.times - 1,
+              execriseTimes: 10,
+              restTimes: 4,
+            };
+          } else {
+            return null;
+          }
+        })
+        ?.filter((ele) => ele);
+
+      setExecriseList(result);
     }, 1000);
 
-    if (progress >= aerobicsList[0].execriseTimes) {
-      execriseList?.filter((item, index) => index !== 0);
+    return () => clearInterval(timer); // 清除上一次的計時器
+  }, [execriseList, isPause]);
 
-      clearInterval(timer);
+  const progressValue = () => {
+    if (!execriseList?.length) return 0;
+
+    if (execriseList[0]?.execriseTimes) {
+      return execriseList[0]?.execriseTimes;
+    } else if (execriseList[0]?.restTimes) {
+      return execriseList[0]?.restTimes;
+    } else {
+      return 0;
     }
+  };
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, [execriseList?.length]);
+  const execriseTitle = () => {
+    if (!execriseList?.length) return "Finish !!!";
+
+    if (execriseList[0]?.execriseTimes) {
+      return `Execrise : ${execriseList[0]?.title} / Left times: ${execriseList[0]?.times}`;
+    } else if (execriseList[0]?.restTimes) {
+      return `${execriseList[0]?.title}: Rest Time`;
+    } else {
+      return "Next Execrise";
+    }
+  };
 
   return (
     <Box sx={{ width: "50%" }}>
-      <LinearProgressWithLabel value={progress} />
+      {execriseTitle()}
+      <LinearProgressWithLabel value={progressValue()} />
+      <Button
+        onClick={() => {
+          setIsPause(!isPause);
+        }}
+      >
+        {isPause ? "Resume" : "Pause"}
+      </Button>
+
+      <Button
+        onClick={() => {
+          setExecriseList(execriseList.slice(1));
+        }}
+      >
+        Skip
+      </Button>
     </Box>
   );
 };
