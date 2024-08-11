07b0fe90cd5441caf36979fa1fced2463fb829e3
Chiyang_Huang <chiyang_huang@wistron.com>
2024-08-11 14:36:25 +0800

    add note about workoutItem Interface

--- app/interface/Workout.ts
+++ app/interface/Workout.ts
@@ -1,14 +1,25 @@
 interface WorkoutItem {
+  // id
   id: number;
+  // 運動名稱
   title: string;
+  // 剩餘運動的時間
   execriseTimes: number;
+  // 剩餘的組數
   set: number;
+  // 剩餘的休息時間
   restTimes: number;
+  // 剩餘的準備時間
   prepareTimes: number;
+  // 規格
   requiredItem: {
+    // 需要的時間
     time: number;
+    // 需要的休息時間
     rest: number;
+    // 需要的準備時間
     prepare: number;
+    // 需要的組數
     set: number;
   };
 }
