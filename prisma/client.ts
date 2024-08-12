9be7c85eb43b171ba0f5924f530b0330773e49c2
Chiyang_Huang <chiyang_huang@wistron.com>
2024-08-12 11:46:19 +0800

    Remove .env file from tracking

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,15 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 
+// 根據環境變數來決定是否初始化 Prisma 客戶端
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return process.env.NEXT_PUBLIC_USE_PRISMA === "true"
+    ? new PrismaClient()
+    : null;
 };
 
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
+// 只在 NEXT_PUBLIC_USE_PRISMA 環境變數設置為 true 的情況下初始化 Prisma
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== "production" && prisma)
+  globalThis.prismaGlobal = prisma;
