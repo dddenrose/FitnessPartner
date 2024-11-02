22c6a7bb10432e138e4b8a37e4612956f5fdbe44
Chiyang_Huang <chiyang_huang@wistron.com>
2024-11-02 16:59:02 +0800

    Brand Story build

--- app/home/components/ThreeImage/page.tsx
+++ app/home/components/ThreeImage/page.tsx
@@ -10,17 +10,33 @@ const ThreeImage = () => {
     <div
       className={classNames(
         "flex",
+        "flex-col",
         "w-full",
         "justify-center",
+        "items-center",
         "pt-12",
         "pb-12",
+        "gap-10",
         "bg-gray-200"
       )}
     >
-      <div className="flex justify-center gap-20">
+      {/* Three Images */}
+      <div className="flex justify-between w-3/5">
         <BorderImage imageSrc={image1} />
         <BorderImage imageSrc={image2} />
         <BorderImage imageSrc={image3} />
+      </div>
+
+      {/* Brand Story */}
+      <div className="flex w-3/5">
+        <div className="text-3xl font-bold flex-1">Sportage Exercise Elite</div>
+        <div className="flex-1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </div>
       </div>
     </div>
   );
