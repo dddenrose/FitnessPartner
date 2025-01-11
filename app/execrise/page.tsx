eb2417cf5048f810a86f2ed1366c542394602994
Chiyang_Huang <chiyang_huang@wistron.com>
2025-01-11 11:42:10 +0800

    feat: 調整導覽列樣式

--- app/execrise/page.tsx
+++ app/execrise/page.tsx
@@ -1,16 +1,21 @@
 "use client";
+import { setTime } from "@/lib/features/execrise/execriseSlice";
 import { RootState } from "@/lib/store";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import ModeButton from "./components/ModeButton";
 import Timer from "./components/Timer";
 
 const Execrise: React.FC = () => {
   const dispatch = useDispatch();
-  const mode = useSelector((state: RootState) => state.execrise.mode);
   const initialTime = useSelector(
     (state: RootState) => state.execrise.initialTime
   );
+
+  React.useEffect(() => {
+    return () => {
+      dispatch(setTime([]));
+    };
+  }, []);
 
   return (
     <div className="min-w-screen min-h-screen">
